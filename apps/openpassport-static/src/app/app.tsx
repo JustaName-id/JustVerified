4e5cafae430884bb64a1d88aacf563a4fc1657da
AngeloAyranji <angeloayranji@gmail.com>
2024-12-10 14:25:49 +0200

    fix(openpassport-static): fix error when generating proof

--- apps/openpassport-static/src/app/app.tsx
+++ apps/openpassport-static/src/app/app.tsx
@@ -2,7 +2,8 @@ import { OpenPassportVerifier } from '@openpassport/core';
 import { OpenPassportQRcode } from '@openpassport/qrcode';
 
 export function App() {
-  const scope = "JustaName | Proof Of Humanity";
+  const scope = "JustaName";
+  // TODO: remove mock passports
   const openPassportVerifier = new OpenPassportVerifier('prove_offchain', scope).allowMockPassports();
   const address = new URLSearchParams(window.location.search).get('address');
   const state = new URLSearchParams(window.location.search).get('state');
@@ -14,8 +15,8 @@ export function App() {
   return (
     <OpenPassportQRcode
       appName={scope}
-      userId={address}
-      userIdType={'hex'}
+      userId={address.replace("0x", "")}
+      userIdType={"hex"}
       openPassportVerifier={openPassportVerifier}
       onSuccess={async (attestation) => {
         console.log(attestation);
